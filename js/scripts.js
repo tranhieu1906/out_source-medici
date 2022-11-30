4a24cc8bf848d3b034f2fe0a6700fe6d731e36d5
philipp-do <quang.do2@sotatek.com>
2022-11-30 15:16:27 +0700

    Add font

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,19 @@
 
 // Calls input from form-input.html
 
-$(document).ready(function(){
 
-  $("#input").submit(function(event){
-    event.preventDefault();
-    var input = ($("#blank").val());
-
-    var output = "";
-
-    $('#output').text(output);
-
-  });
+$(document).ready(function () {
+  var users = ["Hà Huy Toàn", "Lại Văn Sơn", "Hà Văn Huy", "Lê Thị Thường", "Vũ Huy Hoàng", "Trịnh Hồng Sơn"]
+  setInterval(() => {
+    var randomUser = Math.floor(Math.random() * 6);
+    if(randomUser >= 0 && randomUser < 6) {
+      var selectedUser = users[randomUser];
+      $("#notiName").text(selectedUser);
+      $("#notiAvatar").text("https://joeschmoe.io/api/v1/random")
+    }
+    $("#myToast").toast("show");
+    setTimeout(() => {
+      $("#myToast").toast("hide");
+    }, 2000)
+  }, 5000)
 });
